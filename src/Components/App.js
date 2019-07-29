94032c87529b68ab57cae945e4f0099fc1e43b61
Oussama <oussama.aitbaziz@gmail.com>
2019-07-29 22:01:22 +0100

    embedded routes and navlinks

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -6,6 +7,7 @@ import axios from 'axios';
 import SearchForm from './SearchForm';
 import Nav from './Nav';
 import Photo from './Photo';
+import NotFound from './NotFound';
 
 
 
@@ -17,9 +19,11 @@ class App extends Component {
   }
 
   fetchData = () => {
-    axios.get('https://api.unsplash.com/photos/random?count=6' + 
+    axios.get('https://api.unsplash.com/photos/random?count=8' + 
     `&client_id=e5fad60ae18ffcbe62cea64dd2f555033cd9bc323c9b07aed4082e2c327b9ac6`)
-      .then( response => this.setState({photos: response.data}))
+      .then( response => this.setState({
+        photos: response.data
+      }))
       .catch( error => {
         console.log('Error fetching and parsing data', error);
       });
@@ -27,16 +31,27 @@ class App extends Component {
   
   componentDidMount() {
     this.fetchData()
-    console.log(this.state.photos)
   }
 
-
-
   render() {
+    console.log(this.state.photos)
     return (
       <div className="container">
-        <SearchForm />
-        <Nav />
+        <BrowserRouter>
+          <SearchForm />
+          <Nav />
+
+
+          <Switch>
+            <Route path="/cats" render={ () => <Photo />} />
+            <Route path="/dogs" render={ () => <Photo />} />
+            <Route path="/computers" render={ () => <Photo />} />
+            <Route component={NotFound} />
+          </Switch>
+
+        </BrowserRouter>
+        
+
       </div>
     )
   }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+const apiKey = '7f884e438ed27d9bc9a6b95f3a60c73a';
+export default apiKey;
