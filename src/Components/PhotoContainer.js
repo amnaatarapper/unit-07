36d7ee082def46bc8146d8051fc2028727762915
Oussama <oussama.aitbaziz@gmail.com>
2019-07-30 06:10:19 +0100

    updated url

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import PhotoElement from './PhotoElement';
 import NotFound from './NotFound';
+import apiKey from '../config';
 
 class PhotoContainer extends Component {
 
@@ -14,7 +15,7 @@ class PhotoContainer extends Component {
     
     fetchData = (query) => {
         axios.get(`https://api.unsplash.com/search/photos?per_page=24&query=${query}` + 
-        `&client_id=e5fad60ae18ffcbe62cea64dd2f555033cd9bc323c9b07aed4082e2c327b9ac6`)
+        `&client_id=${apiKey}`)
           .then( response => this.setState({ 
             photos: response.data.results,
             isLoading: false }))
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const apiKey = '7f884e438ed27d9bc9a6b95f3a60c73a';
+const apiKey = 'e5fad60ae18ffcbe62cea64dd2f555033cd9bc323c9b07aed4082e2c327b9ac6';
 export default apiKey;
